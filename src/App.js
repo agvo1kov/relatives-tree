ea5608116c0dbe2a72fa93e9bc4a4bd353740c9f
Alexander Volkov <agvolkov4@yandex.ru>
2019-04-30 14:45:54 +0300

    first tree

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ForceGraph2D, ForceGraph3D, ForceGraphVR } from 'react-force-graph';
+import SpriteText from 'three-spritetext';
+// import
+import data from './data.json';
 
-function App() {
-  return (
-    <div className="App">
-      Hello
-    </div>
-  );
+class App extends React.Component {
+    componentDidMount() {
+        // Deactivate existing forces
+        this.fg.d3Force('charge').strength(-400);
+        // Add collision and bounding box forces
+        // this.fg.d3Force('collide', d3.forceCollide(4));
+    }
+    render() {
+        return (
+            <div className="App">
+                <ForceGraph2D
+                    ref={el => this.fg = el}
+                    graphData={data}
+                    nodeRelSize={3}
+                    d3Force={("link.distance", 100)}
+                    nodeAutoColorBy="group"
+                    nodeThreeObject={node => {
+                        const sprite = new SpriteText(node.id);
+                        sprite.color = node.color;
+                        sprite.textHeight = 8;
+                        return sprite;
+                    }}
+                    dagMode={"td"}
+                    dagLevelDistance={80}
+                />
+                });
+            </div>
+        );
+    }
 }
 
 export default App;
